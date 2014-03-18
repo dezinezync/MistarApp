234c91f8b047383db03a12d14b188492a0920016
AndrewSB <asbreckenridge@me.com>
2014-03-17 22:11:03 -0400

    init

---
+++ MistarApp/MistarApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MistarApp
+//
+//  Created by Andrew Breckenridge on 3/17/14.
+//  Copyright (c) 2014 Andrew Breckenridge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MAAppDelegate class]));
+    }
+}
