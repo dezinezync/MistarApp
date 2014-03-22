cc33aa30ef606c0a026112a71a37d18979066a97
AndrewSB <asbreckenridge@me.com>
2014-03-22 11:35:59 -0400

    Included php mister login file

---
+++ MistarApp/mistarPHP.php
@@ -0,0 +1,5 @@
+<form action="https://mistar.oakland.k12.mi.us/novi/StudentPortal/Home/Login" method="post">
+<input type=text name="Pin">
+<input type=password name="Password">
+<input type="submit" id="LoginButton">
+</form>
